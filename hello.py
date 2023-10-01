4b67166923e22093a1bb0c6bc01c90510adf3cc2
João Cardoso <aspirantevitor@gmail.com>
2023-10-01 02:29:38 +0000

    hello world multi language

--- hello.py
+++ hello.py
@@ -1,6 +1,46 @@
-def printando_nome_maiusculo(nome):
-    print(nome.capitalize())
+#!/usr/bin/env python3
+"""Hello World multi-languages
 
-breakpoint()
+Depending on the language configured in the environment, the program displays
+the corresponding message.
 
-printando_nome_maiusculo('hello, world!')
+HOW TO USE:
+Have the `LANG` variable properly configured, i.e.:
+
+    $ export LANG=pt_BR
+
+HOW TO RUN:
+    python3 hello.py
+    OR
+    ./hello.py
+    OR
+    LANG=pt_BR ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__  = "João Cardoso"
+__licence__ = "Unlicence"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+def printing_upper_word():
+    if current_language == "pt_BR":
+        message = "olá, mundo!"
+        print(message.capitalize())
+    elif current_language == "it_IT":
+        message = "ciao, mondo!"
+        print(message.capitalize())
+    elif current_language == "es_SP":
+        message = "hola, mundo!"
+        print(message.capitalize())
+    elif current_language == "fr_FR":
+        message = "bonjour, monde!"
+        print(message.capitalize())
+    else:
+        message = "hello, world!"
+        print(message.capitalize())
+
+if __name__ == "__main__":
+    printing_upper_word()
